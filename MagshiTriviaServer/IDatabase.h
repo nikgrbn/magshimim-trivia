256f4cc1d8dbbd465c123cdd69fa98ba43899296
Nikita Grebenchuk <rmaps123@yandex.ru>
2021-04-26 20:12:22 +0300

    Minor arguments changes

--- MagshiTriviaServer/IDatabase.h
+++ MagshiTriviaServer/IDatabase.h
@@ -5,6 +5,6 @@
 class IDatabase {
 public:
 	virtual bool doesUserExists(std::string username) = 0;
-	virtual bool doesPasswordMatch(std::string original_password, std::string input_password) = 0;
-	virtual void addNewUser(std::string password, std::string username, std::string email) = 0;
+	virtual bool doesPasswordMatch(std::string username, std::string password) = 0;
+	virtual void addNewUser(std::string username, std::string password, std::string email) = 0;
 };
