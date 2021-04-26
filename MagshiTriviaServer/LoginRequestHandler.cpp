3afc75c0e25b5c7573e6da33a66bd18901dd60f2
Ori <literallynotf@gmail.com>
2021-04-26 18:49:57 +0300

    Updated tests of requests handler

--- MagshiTriviaServer/LoginRequestHandler.cpp
+++ MagshiTriviaServer/LoginRequestHandler.cpp
@@ -8,15 +8,20 @@ RequestResult LoginRequestHandler::handleRequest(RequestInfo info) {
 	RequestResult response;
 
 	// -- Tests --
-
-	LoginRequest l = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
-	LoginResponse r;
-	r.status = 100;
-
-	// ...
-
-	response.buffer = JsonResponsePacketSerializer::serializeResponse(r);
-	response.newHandler = nullptr;
-
+	if (info.id == ProtocolCodes::Login) {
+		LoginRequest l = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
+		LoginResponse r;
+		r.status = 100;
+		response.buffer = JsonResponsePacketSerializer::serializeResponse(r);
+		response.newHandler = nullptr;
+	} else if (info.id == ProtocolCodes::Signup) {
+		LoginRequest l = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
+		SignupResponse r_s;
+		r_s.status = 100;
+		response.buffer = JsonResponsePacketSerializer::serializeResponse(r_s);
+		response.newHandler = nullptr;
+	}
 	// -- Tests --
+
+	return response;
 }
--- client_test.py
+++ client_test.py
@@ -4,9 +4,33 @@ import socket
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8728        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    data = s.recv(1024)
-    s.sendall(b'Hello')
+def communicate_with_server():
+    try:
+        s.connect((HOST, PORT))
+    except Exception as e:
+        print("* Connection failed.")
+        exit(1)
 
-print('Received', repr(data))
+    print("* Connection succeeded \n")
+
+	json_dict = {"username" : 'user1', "password" : '123', "Email" : '123@123'}
+	string_json = json.dumps(json_dict)
+	str_send = chr(1) + string_json
+
+	sock.send(str_send.encode())
+
+
+	data = sock.recv(1024)
+	print(data)
+
+	input()
+
+	sock.close()
+
+
+
+def main():
+	communicate_with_server()
+
+if __name__ == "__main__":
+	main()
