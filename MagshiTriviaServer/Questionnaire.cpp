c7fdf32ea0e558f0f5d11d870b13b9158c30d2a9
Nikita Grebenchuk <rmaps123@yandex.ru>
2021-05-28 13:24:12 +0300

    Initialized basic question database support

--- MagshiTriviaServer/Questionnaire.cpp
+++ MagshiTriviaServer/Questionnaire.cpp
@@ -2,9 +2,88 @@
 
 Questionnaire::Questionnaire()
 {
+    this->questions.push_back(Question(
+        "What year was the very first model of the iPhone released?",
+        "2011",
+        "1998",
+        "2007",
+        "2003",
+        3));
+
+    this->questions.push_back(Question(
+        "What’s the shortcut for the “copy” function on most computers?",
+        "ctrl+c",
+        "ctrl+x",
+        "shift+f4",
+        "alt+tab",
+        1));
+
+    this->questions.push_back(Question(
+        "Which programming language shares its name with an island in Indonesia?",
+        "Python",
+        "Jakarta",
+        "Java",
+        "Kotlin",
+        3));
+
+    this->questions.push_back(Question(
+        "Which of these programming languages is a low-level language?",
+        "Rockstar",
+        "C++",
+        "Python",
+        "Assembley",
+        4));
+
+    this->questions.push_back(Question(
+        "Generally, which component of a computer draws the most power?",
+        "Video Card",
+        "Mother Board",
+        "Processor",
+        "Power Supply",
+        1));
+
+    this->questions.push_back(Question(
+        "When Gmail first launched, how much storage did it provide for your email?",
+        "512MB",
+        "1GB",
+        "5GB",
+        "Unlimited",
+        2));
+
+    this->questions.push_back(Question(
+        "All of the following programs are classified as raster graphics editors EXCEPT:",
+        "Paint",
+        "GIMP",
+        "Adobe Photoshop",
+        "Inkscape",
+        4));
+
+    this->questions.push_back(Question(
+        "According to the International System of Units, how many bytes are in a kilobyte of RAM?",
+        "512",
+        "64",
+        "1000",
+        "1024",
+        3));
+
+    this->questions.push_back(Question(
+        "HTML is what type of language?",
+        "Macro Language",
+        "Programming Language",
+        "Scripting Language",
+        "Markup Language",
+        4));
+
+    this->questions.push_back(Question(
+        "In programming, what do you call functions with the same name but different implementations?",
+        "Overloading",
+        "Overriding",
+        "Abstracting",
+        "Inherting",
+        1));
 }
 
 std::list<Question> Questionnaire::getQuestionList()
 {
-    return std::list<Question>();
+    return this->questions;
 }
--- MagshiTriviaServer/Questionnaire.h
+++ MagshiTriviaServer/Questionnaire.h
@@ -3,10 +3,22 @@
 #include <iostream>
 #include <list>
 
+
 typedef struct Question {
 	std::string question;
 	std::string answers[4];
 	unsigned int correct_ans;
+
+	Question();
+	Question(std::string qu, std::string a1, std::string a2, std::string a3, std::string a4, unsigned int cor_ans)
+	{
+		question = qu;
+		answers[0] = a1;
+		answers[1] = a2;
+		answers[2] = a3;
+		answers[3] = a4;
+		correct_ans = cor_ans;
+	}
 }; Question;
 
 class Questionnaire {
