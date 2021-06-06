34e37040adcd0ae46c023f8b04609ddd72235c47
Nikita Grebenchuk <rmaps123@yandex.ru>
2021-06-06 11:02:47 +0300

    feat: added config file to client

--- MagshiTriviaClient/Logic/Communicator.cs
+++ MagshiTriviaClient/Logic/Communicator.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Windows.Threading;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
+using System.Reflection;
 
 namespace MagshiTriviaClient
 {
@@ -24,7 +26,24 @@ namespace MagshiTriviaClient
         }
         public void ConnectToServer()
         {
-            socket.Connect("127.0.0.1", 8728);
+            // Find config file in a directory
+            string config = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "config.txt");
+            if (!File.Exists(config))
+            {
+                config = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\config.txt"));
+                if (!File.Exists(config))
+                {
+                    throw new DirectoryNotFoundException("Can't locate a config file!");
+                }
+            }
+
+            // Read a text file line by line.  
+            string[] lines = File.ReadAllLines(config);
+            string ip = (lines[0].Split('='))[1];
+            int port = Convert.ToInt32((lines[1].Split('='))[1]);
+
+            // Connect to a server
+            socket.Connect(ip, port);
         }
 
         public string sendPacketToServer(byte[] packet)
