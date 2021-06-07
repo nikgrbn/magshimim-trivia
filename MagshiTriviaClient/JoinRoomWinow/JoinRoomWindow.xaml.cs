233a92d805ebfb0e4d808b394ee86dda03851e7c
Ori <literallynotf@gmail.com>
2021-06-07 17:57:04 +0300

    build: add new folder and new files

---
+++ MagshiTriviaClient/JoinRoomWinow/JoinRoomWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MagshiTriviaClient.JoinRoomWinow
+{
+    /// <summary>
+    /// Interaction logic for JoinRoomWindow.xaml
+    /// </summary>
+    public partial class JoinRoomWindow : Window
+    {
+        public JoinRoomWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
