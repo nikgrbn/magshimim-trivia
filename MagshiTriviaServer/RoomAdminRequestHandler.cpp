f0bb3fdd2b9bf0a826452a6ae5aed329da11a0a4
Ori <literallynotf@gmail.com>
2021-06-09 12:52:13 +0300

    feat: added request handlers for close room and start game

--- MagshiTriviaServer/RoomAdminRequestHandler.cpp
+++ MagshiTriviaServer/RoomAdminRequestHandler.cpp
@@ -1,5 +1,75 @@
 #include "RoomAdminRequestHandler.h"
+#include "MenuRequestHandler.h"
 
 RoomAdminRequestHanlder::~RoomAdminRequestHanlder()
+{ }
+
+bool RoomAdminRequestHanlder::IsRequestRelevant(RequestInfo info) {
+	return (info.id == ProtocolCodes::CloseRoomRequest || info.id == ProtocolCodes::StartGameRequest || info.id == ProtocolCodes::GetRoomStateRequest);
+}
+
+RequestResult RoomAdminRequestHanlder::handleRequest(RequestInfo info) {
+	RequestResult response;
+
+	switch (info.id)
+	{
+	case ProtocolCodes::CloseRoomRequest: {
+		response = this->closeRoom(info);
+		break;
+	}
+
+	case ProtocolCodes::StartGameRequest: {
+		response = this->startGame(info);
+		break;
+	}
+	
+	case ProtocolCodes::GetRoomStateRequest: {
+		response = this->getRoomState(info);
+		break;
+	}
+
+	default:
+		break;
+	}
+
+	return response;
+}
+
+RequestResult RoomAdminRequestHanlder::closeRoom(RequestInfo info) {
+	RequestResult response{};
+	CloseRoomResponse close_room_response{};
+
+	try {
+		this->_room_manager->deleteRoom(this->_room.getRoomData().id);
+		response.newHandler = this->_handle_factory->createMenuRequestHandler(this->_user);
+		close_room_response.status = ResponseStatus::CloseRoomRequestSuccess;
+	} catch (std::exception& e) {
+		response.newHandler = this;
+		close_room_response.status = ResponseStatus::CloseRoomRequestSuccess;
+	}
+
+	response.buffer = JsonResponsePacketSerializer::serializeResponse(close_room_response);
+	return response;
+}
+
+RequestResult RoomAdminRequestHanlder::startGame(RequestInfo info) {
+	RequestResult response{};
+	StartGameResponse start_game_response{};
+
+	try {
+		this->_room.activateRoom();
+		response.newHandler = this;
+		start_game_response.status = ResponseStatus::StartGameRequestSuccess;
+	} catch (std::exception& e) {
+		response.newHandler = this;
+		start_game_response.status = ResponseStatus::StartGameRequestError;
+	}
+
+	response.buffer = JsonResponsePacketSerializer::serializeResponse(start_game_response);
+	return response;
+}
+
+RequestResult RoomAdminRequestHanlder::getRoomState(RequestInfo info)
 {
+	return RequestResult();
 }
--- MagshiTriviaServer/RoomAdminRequestHandler.h
+++ MagshiTriviaServer/RoomAdminRequestHandler.h
@@ -3,6 +3,8 @@
 // Project header files
 #include "IRequestHandler.h"
 #include "RoomManager.h"
+#include "Room.h"
+#include "LoggedUser.h"
 #include "JsonResponsePacketSerializer.h"
 #include "RequestHandlerFactory.h"
 
@@ -11,6 +13,16 @@ class RoomAdminRequestHanlder : public IRequestHandler
 public:
 	~RoomAdminRequestHanlder();
 
-private:
+	bool IsRequestRelevant(RequestInfo info) override;
+	RequestResult handleRequest(RequestInfo info) override;
 
+private:
+	Room _room;
+	LoggedUser _user;
+	RoomManager* _room_manager;
+	RequestHandlerFactory* _handle_factory;
+
+	RequestResult closeRoom(RequestInfo info);
+	RequestResult startGame(RequestInfo info);
+	RequestResult getRoomState(RequestInfo info);
 };
