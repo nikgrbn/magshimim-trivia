3b0b1a8ffe604d97c726961afada5e9e00b469b5
Nikita Grebenchuk <rmaps123@yandex.ru>
2021-06-24 01:16:18 +0300

    other: changed [] operator to .at() method

--- MagshiTriviaServer/Game.cpp
+++ MagshiTriviaServer/Game.cpp
@@ -5,7 +5,9 @@ Game::Game(std::vector<LoggedUser> users, unsigned int maxQuestions, IDatabase* 
 {
 	// Push random questions from database to vector
 	std::list<Question> questionsList = this->_db->getQuestions(this->_maxQuestions);
-	std::copy(questionsList.begin(), questionsList.end(), std::back_inserter(this->_questions));
+	for (auto q : questionsList) {
+		this->_questions.push_back(q);
+	}
 
 	// Create startup game data structure
 	GameData templateData(this->_questions[0], 0, 0, 0);
@@ -25,10 +27,12 @@ Game::Game(std::vector<LoggedUser> users, unsigned int maxQuestions)
 
 	// Push random questions from database to vector
 	std::list<Question> questionsList = this->_db->getQuestions(this->_maxQuestions);
-	std::copy(questionsList.begin(), questionsList.end(), std::back_inserter(this->_questions));
+	for (auto q : questionsList) {
+		this->_questions.push_back(q);
+	}
 
 	// Create startup game data structure
-	GameData templateData (this->_questions[0], 0, 0, 0);
+	GameData templateData (this->_questions.at(0), 0, 0, 0);
 
 	// Create map of users and data
 	for (auto user : users)
@@ -40,6 +44,31 @@ Game::Game(std::vector<LoggedUser> users, unsigned int maxQuestions)
 Question Game::getQuestionForUser(LoggedUser user)
 {
 	std::map<LoggedUser, GameData>::iterator it = this->_players.find(user);
-
 	return it->second.currentQuestion;
+}
+
+void Game::submitAnswer(LoggedUser user, unsigned int answer)
+{
+	std::map<LoggedUser, GameData>::iterator it = this->_players.find(user);
+
+	// If answer is correct
+	if (answer == it->second.currentQuestion.correct_ans)
+	{
+		// Update data
+		it->second.correctAnswerCount += 1;
+		it->second.currentQuestion = this->_questions.at(it->second.correctAnswerCount + it->second.wrongAnswerCount);
+	}
+	// If answer is incorrect
+	else
+	{
+		// Update data
+		it->second.wrongAnswerCount += 1;
+		it->second.currentQuestion = this->_questions.at(it->second.correctAnswerCount + it->second.wrongAnswerCount);
+	}
+}
+
+void Game::removePlayer(LoggedUser user)
+{
+	std::map<LoggedUser, GameData>::iterator it = this->_players.find(user);
+	this->_players.erase(it);
 }
